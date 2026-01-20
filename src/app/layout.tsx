de0a93fafb502d3447902eadbdfe8ac62e182826
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2026-01-20 23:35:43 +0000

    fix: correct typo in Grow Site ID (I -> l)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -175,7 +175,7 @@ export default function RootLayout({
         <script
           data-grow-initializer=""
           dangerouslySetInnerHTML={{
-            __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2I0ZTo2NWU3MDY3YS0wNzI0LTQ0NjktYTZmYS1iYzU0YWYzNWYzOTg=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`,
+            __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTo2NWU3MDY3YS0wNzI0LTQ0NjktYTZmYS1iYzU0YWYzNWYzOTg=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`,
           }}
         />
 
