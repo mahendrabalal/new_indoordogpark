f1b3afb0a12cbc91dbcc572a0fc98536e23e4673
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2026-02-06 03:07:39 +0000

    feat: split bundle into admin/cms/default functions to fit 25MB limit

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1770345821510,
+    __BUILD_TIMESTAMP_MS__: 1770347213624,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
